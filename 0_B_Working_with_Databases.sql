06c13facaca2a753edaa1e8eccc729e765fb91e0
Dheeraj Tuteja <44654396+dheerajtuteja@users.noreply.github.com>
2020-09-24 13:43:49 +0200

    Add files via upload

---
+++ 0_B_Working_with_Databases.sql
@@ -0,0 +1,36 @@
+/* CREATE DATABASE
+
+--> There are 2 ways to Create Database in SQL server.
+
+1.) SQL Server Management Studio (Right Click on Database and click New Database)  
+
+(2.) Transact-SQL- Create Database with Transact-SQL: */
+
+
+CREATE DATABASE MOVIES_Test
+
+/* ALTER DATABASE
+
+The ALTER DATABASE statement must run in an auto-commit mode.
+ALTER DATABASE is not allowed in an explicit or implicit transaction.
+
+--> There are 2 ways to Alter Database in SQL server.
+
+1.) SQL Server Management Studio (Right Click on Database and Rename)  
+
+(2.) Transact-SQL- Alter Database with Transact-SQL: */
+
+
+ALTER DATABASE [TSQLV4]              
+MODIFY NAME = [TSQLV5]
+
+
+/* DELETE DATABASE
+
+There are 2 ways to Delete Database in SQL server.
+
+1.) SQL Server Management Studio (Right Click and Delete Database)
+
+(2.) Transact-SQL - Delete Database with Transact-SQL: */
+
+DROP DATABASE [TSQLV5]
