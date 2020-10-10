e7b6cd0b6cd66f8d6267cef730153d1d5030c8a7
Dheeraj Tuteja <44654396+dheerajtuteja@users.noreply.github.com>
2020-10-10 13:07:30 +0200

    Add files via upload

---
+++ 25_Page_Split_8KB_Page.sql
@@ -0,0 +1,3 @@
+-- https://www.youtube.com/watch?v=He8MRttysmY
+-- http://naveenkumartblog.blogspot.com/2014/11/prove-one-page-8-kb-in-sql-server.html
+-- http://etutorials.org/SQL/microsoft+sql+server+2000/Part+V+SQL+Server+Internals+and+Performance+Tuning/Chapter+33.+SQL+Server+Internals/Database+Pages/
