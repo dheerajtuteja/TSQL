6c17c09ff2d5148ba9202d35e89f25f8e5a2c231
Dheeraj Tuteja <44654396+dheerajtuteja@users.noreply.github.com>
2020-10-10 12:29:42 +0200

    Add files via upload

---
+++ 23_Cursors.sql
@@ -0,0 +1,3 @@
+-- https://www.youtube.com/watch?v=IIbwLr0v7LA&list=PLVlQHNRLflP8WFEvFvANnUsD2y8HJIJh1&index=46
+-- https://www.youtube.com/watch?v=yK8epOlliU4&list=PLVlQHNRLflP8WFEvFvANnUsD2y8HJIJh1&index=47
+-- https://www.sqlservertutorial.net/sql-server-stored-procedures/sql-server-cursor/
